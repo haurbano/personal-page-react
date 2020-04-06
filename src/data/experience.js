ded604fe13f75e3cc65b510acb297c4214fd2626
Hamilton Urbano <haurbano.dev@gmail.com>
2020-04-05 21:07:30 -0500

    Experiene json file completed

--- src/data/experience.js
+++ src/data/experience.js
@@ -1,21 +1,126 @@
 [
     { 
-        "title":"",
+        "title":"Android Tech Lead",
         "company": {
-            "name": "",
-            "link": ""
+            "name": "Globant",
+            "link": "https://www.globant.com/"
+        },
+        "proyect": {
+            "name": "Royal Caribbean International",
+            "link": "https://play.google.com/store/apps/details?id=com.rccl.royalcaribbean&hl"
+        },
+        "date": "October 2019 - Present",
+        "resume": "My role as an Android Tech Lead is coordinate planning and delivery new features. Some of my responsibilities are:",
+        "resposabilities": [
+            "Technical design of new features and review technical designs from others, always looking for scalability and sustainability inside the project. Also, expose these technical designs to the consumers and negotiate times for its implementation.",
+            "Keep the quality of the project using code-review, unit testing, and any kind of tool that could help to keep and improve the quality of the project",
+            "Planning and estimate epics, tasks and features in order to complete successfully every sprint.",
+            "Training, mentoring and coaching the developers to keep the whole team in a motivated environment.",
+            "Provide everything to the developers needs to complete their task in every sprint."
+        ],
+        "technologies": [
+            "Android",
+            "Clean Architecture",
+            "Kotlin",
+            "Team Managment",
+            "MVVM"
+        ]
+    },
+
+    { 
+        "title":"Android Developer",
+        "company": {
+            "name": "Globant",
+            "link": "https://www.globant.com/"
+        },
+        "proyect": {
+            "name": "Royal Caribbean International",
+            "link": "https://play.google.com/store/apps/details?id=com.rccl.royalcaribbean&hl"
+        },
+        "date": "August 2018 - October 2019",
+        "resume": "",
+        "resposabilities": [
+            "Thech design for small features",
+            "Developr new features",
+            "Unit Testing",
+            "Code Review"
+        ],
+        "technologies": [
+            "Android",
+            "Clean Architecture",
+            "Kotlin",
+            "Java",
+            "Dependecy Injection with Dagger 2",
+            "RxJava",
+            "SOLID principles",
+            "MVP"
+        ]
+    },
+
+    { 
+        "title":"Senior Android Developer",
+        "company": {
+            "name": "Grability",
+            "link": "https://www.grability.com/"
+        },
+        "proyect": {
+            "name": "Walmart Mexico",
+            "link": "https://play.google.com/store/apps/details?id=com.superama15.screens"
+        },
+        "date": "January 2018 - August 2018",
+        "resume": "As an Android Developer I was mainly in charge of the design, implement new features, bugs fixing and coordinate the APIs with the backend team",
+        "resposabilities": [],
+        "technologies": [
+            "Android",
+            "Kotlin",
+            "Java",
+            "Dependecy Injection with Dagger 2",
+            "RxJava",
+            "MVP"
+        ]
+    },
+
+    { 
+        "title":"Product Engineer",
+        "company": {
+            "name": "Codescrum",
+            "link": "https://codescrum.com/"
         },
         "proyect": {
             "name": "",
             "link": ""
         },
-        "date": "",
-        "resume": "",
-        "resposabilities": [
-
-        ],
+        "date": "January 2017 - December 2017",
+        "resume": "In CodeScrum I was in charge of a full projects, from take the requirements to deploy the new functionalities",
+        "resposabilities": [],
         "technologies": [
-            
+            "Ruby on Rails",
+            "MySQL",
+            "MongoDB",
+            "RSpec",
+            "Docker",
+            "Android",
+            "Kotlin"
         ]
-    }
+    },
+
+    { 
+        "title":"Technical advisor",
+        "company": {
+            "name": "Cluster Creatic",
+            "link": "https://clustercreatic.com/es/"
+        },
+        "proyect": {
+            "name": "",
+            "link": ""
+        },
+        "date": "May 2015 - Julay 2012",
+        "resume": "As an Technical advisor I was in charge of help start-ups developing their technology product. I had the oportunity of learn frmo several technologies in the backend, frontend and even mobile side.",
+        "resposabilities": [],
+        "technologies": [
+            "Android",
+            "Angular 1",
+            "Ruby on Rails"
+        ]
+    },
 ]
