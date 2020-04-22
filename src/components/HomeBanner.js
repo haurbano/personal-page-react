222398a675e50edeb8e17b237f54d936b0a08e41
Hamilton Urbano <haurbano.dev@gmail.com>
2020-04-21 21:32:31 -0500

    Progress on the look and fill of banner, missing impor awesome font

--- src/components/HomeBanner.js
+++ src/components/HomeBanner.js
@@ -15,17 +15,17 @@ function HomeBanner() {
 							<div class="media-body">
 								<div class="personal_text">
 									<h6>Hello Everybody, i am</h6>
-									<h3>Donald McKinney</h3>
-									<h4>Junior UI/UX Developer</h4>
-									<p>You will begin to realise why this exercise is called the Dickens Pattern (with reference to the ghost showing Scrooge some different futures)</p>
+									<h3>Hamilton Urbano</h3>
+									<h4>Sr. Android Developer</h4>
+									<p>A lover for technology with passion for the well implemented software and architectures. I'm always looking for improve and be up to date about the latest technologies.</p>
 									<ul class="list basic_info">
-										<li><a href="#"><i class="lnr lnr-calendar-full"></i> 31st December, 1992</a></li>
-										<li><a href="#"><i class="lnr lnr-phone-handset"></i> 44 (012) 6954 783</a></li>
-										<li><a href="#"><i class="lnr lnr-envelope"></i> businessplan@donald</a></li>
-										<li><a href="#"><i class="lnr lnr-home"></i> Santa monica bullevard</a></li>
+										<li><a href="#"><i class="lnr lnr-calendar-full"></i> 8 Septembre, 1993</a></li>
+										<li><a href="#"><i class="lnr lnr-phone-handset"></i> (+57) 3108402718</a></li>
+										<li><a href="#"><i class="lnr lnr-envelope"></i> haurbano.dev@gmail.com</a></li>
+										<li><a href="#"><i class="lnr lnr-home"></i> Bogotá, Colombia</a></li>
 									</ul>
 									<ul class="list personal_social">
-										<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
 										<li><a href="#"><i class="fa fa-twitter"></i></a></li>
 										<li><a href="#"><i class="fa fa-linkedin"></i></a></li>
 									</ul>
