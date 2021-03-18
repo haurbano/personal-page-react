a401e2cafacc7de1bb489bd64d2195614c33381c
Hamilton Urbano <haurbano.dev@gmail.com>
2021-03-17 22:06:05 -0500

    English review

--- src/components/background/CareerSummary.js
+++ src/components/background/CareerSummary.js
@@ -8,7 +8,7 @@ function CareerSummary() {
         <section class="resume-section text-left summary-section mb-5">
             <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">Career Summary</h2>
             <div class="resume-section-content">
-                <p class="mb-0">I'm Hamilton, a lover for technology with passion for the well implemented software and architectures. I'm always looking for improve and be up to date about the latest technologies.</p>
+                <p class="mb-0">I'm Hamilton, a technology lover with a passion for well-implemented software and architectures. I'm always looking for improvements and being up to date about the latest technologies.</p>
             </div>
         </section>
     );
